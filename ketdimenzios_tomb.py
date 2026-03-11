dff361e409c2927c653c8d2dc60939d0d4c70300
Gerinova01 <kullai.gergo@diak.szbi-pg.hu>
2026-03-11 10:46:57 +0100

    asfwawe

---
+++ ketdimenzios_tomb.py
@@ -0,0 +1,18 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# # print(matrix[2][1])
+# for sor in matrix:
+#      print(sor)
+#     for oszlop in sor:
+#         print(oszlop, end=" ")
+#     print()
+
+
+# csak szabályos 3*3 5*5
+for i in range(0, len(matrix)):
+    for j in range(0, len(matrix)):
+        print(matrix[i][j], end= " ")
---
+++ létrehozas.py
@@ -0,0 +1,13 @@
+tomb = []
+
+
+sor = 4
+oszlop = 3
+
+for i in range(sor):
+    sor_tomb = []
+    for j in range(oszlop):
+        sor_tomb.append(None)
+    tomb.append(sor_tomb)
+
+print(tomb)
